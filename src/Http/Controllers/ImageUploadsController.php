5efa71a9c667a02e75f7866ef50fdc99be3eca2d
Oluwaseyi Adeogun <sheyilaaw98@gmail.com>
2020-04-13 14:55:31 +0100

    Add cloudinary support for image upload

--- src/Http/Controllers/ImageUploadsController.php
+++ src/Http/Controllers/ImageUploadsController.php
@@ -3,6 +3,7 @@
 namespace Wink\Http\Controllers;
 
 use Illuminate\Support\Facades\Storage;
+use JD\Cloudder\Facades\Cloudder;
 
 class ImageUploadsController
 {
@@ -19,8 +20,15 @@ class ImageUploadsController
             ]
         );
 
+        $image_url = Storage::disk(config('wink.storage_disk'))->url($path);
+
+        if( env('CLOUDINARY_UPLOAD') ) {
+            Cloudder::upload(request()->image->path(), null);
+            $image_url = Cloudder::show(Cloudder::getPublicId(), []);
+        }
+
         return response()->json([
-            'url' => Storage::disk(config('wink.storage_disk'))->url($path),
+            'url' => $image_url
         ]);
     }
 }
